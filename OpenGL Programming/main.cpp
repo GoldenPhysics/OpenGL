6427140ccad82ff70087abd9148e6f92239a34ab
Ellis Barnes <EllisBarnes00@gmail.com>
2017-07-25 16:47:36 -0400

    Drew first triangle
    I am so confused

--- OpenGL Programming/main.cpp
+++ OpenGL Programming/main.cpp
@@ -4,9 +4,14 @@
 #include "input/input.h"
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
+	-0.5f, -0.5f, 0.0f, // Left		0
+	0.5f, -0.5f, 0.0f,	// Right	1
+	0.0f,  0.5f, 0.0f	// Top		2
+};
+
+unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 2  // first Triangle
+	//1, 2, 3   // second Triangle
 };
 
 const char *vertex_shader_source = "#version 330 core\n"
@@ -51,20 +56,49 @@ int main(int argc, char *argv[])
 	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
 	std::cout << "Shader Program Status: " << success << std::endl;
 
-	// Use the program and clean up after ourselves
+	// Delete old shaders
 	glDeleteShader(vertex_shader | fragment_shader);
-	glUseProgram(shader_program);
 
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// Wireframe
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (true)
 	{
 		input::handle_input(win);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader_program);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
 		SDL_GL_SwapWindow(win.m_window); // This handles swapping buffers
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	return 0;
 }
